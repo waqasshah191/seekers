be44448bcb7fb36a42e78eebd93be38c1f22245f
Ray Taghavi <65364572+raytaghavi@users.noreply.github.com>
2021-06-24 15:14:16 -0600

    create React Skeleton App

---
+++ hello-world/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+// Removed logo since we don't need it anymore
+import './App.css';
+class App extends Component {
+  render() {
+    return (
+      <div classname="container">
+        <h1>Hello World!</h1>
+        <p>Ray just created the project 3's React App</p>
+      </div>
+    );
+  }
+}
+
+export default App;
