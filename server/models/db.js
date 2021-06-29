948aaef0b9b4721f8f749c718682a0c0bb43d37d
tonyyu888 <antonio.yu@gmail.com>
2021-06-29 11:07:07 -0600

    Moved connection string to env

--- server/models/db.js
+++ server/models/db.js
@@ -1,23 +1,21 @@
 const mongoose = require('mongoose');
 
 /* Moved credentials to .env */
-
-// const mongoUser = 'dbReadOnlyUser';
-// const mongoPasswd = 'jelly1234';
-// const mongoDBName = 'MERN-STARTER-DB';
-// const mongoServer = 'cluster0.vvqav.mongodb.net';
-// const url =
-//   `mongodb+srv://${mongoUser}:${mongoPasswd}` +
-//   `@${mongoServer}/${mongoDBName}?retryWrites=true&w=majority`;
+//  const mongoUser = 'sumi';
+//  const mongoPasswd = 'sumi';
+//  const mongoDBName = 'POS';
+//  const mongoServer = 'cluster0.bixpu.mongodb.net';
+//  const url =
+//    `mongodb+srv://${mongoUser}:${mongoPasswd}` +
+//    `@${mongoServer}/${mongoDBName}?retryWrites=true&w=majority`;
 
 // const localMongoUrl = "mongodb://localhost:27017/c6Superheroes"
+//mongoose.connect("mongodb://localhost:27017/POS", { useNewUrlParser: true, useUnifiedTopology: true });
 
-//mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connect("mongodb://localhost:27017/POS", { useNewUrlParser: true, useUnifiedTopology: true });
-
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.once('open', (_) =>
-console.log('MongoDB is now connected:')  
-// console.log('MongoDB is now connected:', process.env.MONGODB_URL)
+//console.log('MongoDB is now connected:')  
+ console.log('MongoDB is now connected:', process.env.MONGODB_URL)
 );
 db.on('error', (err) => console.error('MongoDB connection error!', err));
