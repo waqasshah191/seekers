098675a492c1b9b9bd70379d3f50b2b9e2689b99
tonyyu888 <antonio.yu@gmail.com>
2021-07-30 11:24:37 -0600

    Modified comments in file

--- server/routes/longLat.js
+++ server/routes/longLat.js
@@ -3,20 +3,22 @@ var router = express.Router();
 
 const https = require('https');
 
-/* Update a supplier by ID. */
+/* Generate long/lat  based on address */
+//Postman: http://localhost:3000/longLat/33 Somercrest Circle SW, Calgary, AB
 router.get('/:address', async function(req, res) {  
-    let supplierToUpdate = req.body
-  
-    const fetch = require('node-fetch');
+
+  const fetch = require('node-fetch');
   
     (async () => {
       try {
     
-        //const response = await fetch('http://geogratis.gc.ca/services/geolocation/en/locate?q=' + supplierToUpdate[0].address1 + ' ' + supplierToUpdate[0].city + ' ' + supplierToUpdate[0].province )
+        // using Govenment of Canada geolocation API
+        // const response = await fetch('http://geogratis.gc.ca/services/geolocation/en/locate?q=' + supplierToUpdate[0].address1 + ' ' + supplierToUpdate[0].city + ' ' + supplierToUpdate[0].province )
         // console.log("json latitude = ", json[0].geometry.coordinates[0])
         // console.log("json longitude = ", json[0].geometry.coordinates[1])
         
-        //works for address
+        // using mapbox geocoding api.  Need to sign up to get a public token to use their api
+        // works for address
         const response = await fetch('https://api.mapbox.com/geocoding/v5/mapbox.places/' + req.params.address  +  '.json?access_token=pk.eyJ1Ijoicmlja3NoYXc4ODgiLCJhIjoiY2tyanlyNWV6MXZxMzMxbzI0bnA3dm5hMyJ9.jmu4biSr9nQwLErIGJeG6A&limit=1')
         const json = await response.json()
         //console.log("json = ", json.features[0].center[0])
