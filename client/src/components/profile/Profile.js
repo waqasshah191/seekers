329f5232fc36ee65906844c4d28ab3355be1c336
waqasshah191 <syedwaqasshah191@gmail.com>
2021-08-04 14:15:01 -0600

    pull refresh

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
+import { Loading } from "../index"
 import { makeStyles } from '@material-ui/core/styles'
 import Grid from '@material-ui/core/Grid'
 import Card from '@material-ui/core/Card'
@@ -83,8 +85,42 @@ const useStyles = makeStyles({
   }
 })
 
-export default function Profile() {
+// const Profile = () => {
+//   const { user } = useAuth0();
+//   const { name, picture, email } = user;
 
+//   return (
+//     <div>
+//       <div className="row align-items-center profile-header">
+//         <div className="col-md-2 mb-3">
+//           <img
+//             src={picture}
+//             alt="Profile"
+//             className="rounded-circle img-fluid profile-picture mb-3 mb-md-0"
+//           />
+//         </div>
+//         <div className="col-md text-center text-md-left">
+//           <h2>{name}</h2>
+//           <p className="lead text-muted">{email}</p>
+//         </div>
+//       </div>
+//       <div className="row">
+//         <pre className="col-12 text-light bg-dark p-4">
+//           {JSON.stringify(user, null, 2)}
+//         </pre>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default withAuthenticationRequired(Profile, {
+//   onRedirecting: () => <Loading />,
+// });
+
+const Profile = () => {
+  const { user } = useAuth0();
+  const { name, picture, email } = user;
+  
   const classes = useStyles()
 
   console.log(`styles: ${JSON.stringify(classes)}`);
@@ -195,3 +231,7 @@ export default function Profile() {
 
   )
 }
+
+export default withAuthenticationRequired(Profile, {
+  onRedirecting: () => <Loading />,
+});
