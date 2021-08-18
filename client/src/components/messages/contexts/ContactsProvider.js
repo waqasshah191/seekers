c88142f691144380d5c71ca84f59f4d044a12d83
tonyyu888 <antonio.yu@gmail.com>
2021-08-18 11:07:51 -0600

    Modified user model to accomodate new socialMediaUrl structure.  Messaging change was for loading users in system.

--- client/src/components/messages/contexts/ContactsProvider.js
+++ client/src/components/messages/contexts/ContactsProvider.js
@@ -1,6 +1,8 @@
 import React, { useContext } from 'react'
 import useLocalStorage from '../hooks/useLocalStorage';
 
+import { useEffect } from 'react';
+
 const ContactsContext = React.createContext()
 
 export function useContacts() {
@@ -9,6 +11,39 @@ export function useContacts() {
 
 export function ContactsProvider({ children }) {
   const [contacts, setContacts] = useLocalStorage('contacts', [])
+
+  const getUsers = async () => {
+    let response = await fetch('/user');
+    let users = await response.json();
+
+    // let prevContacts = [...contacts];
+    let prevContacts = [];
+    let userEmail = '';
+    let userFirstName = '';
+    let userLastName = '';
+    let userName = '';
+
+    console.log("users = ", users);
+
+    for(let j=0; j<users.length; j++) {
+      userEmail = users[j].email;
+      userFirstName = users[j].firstName;
+      userLastName = users[j].lastName;
+      userName = userLastName ? userFirstName + ' ' + userLastName : userFirstName;
+  
+      prevContacts.push({id: userEmail, name: userName})
+    }
+  
+    console.log("prevContacts = ", prevContacts)
+  
+    setContacts(prevContacts);
+  };
+  
+  useEffect(() => {
+
+      getUsers();
+
+  }, []);
 
   function createContact(id, name) {
     setContacts(prevContacts => {
